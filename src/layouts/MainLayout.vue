7940f1dea86f45bb58188205fdc270cb23f9d60e
Fedorov Nikita <fedorovni1996@yandex.ru>
2020-04-09 17:48:51 +0500

    Добавил обработку ошибок на MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,12 +13,23 @@
 <script>
 import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
+import messages from '@/utils/messages'
 
 export default {
   name: 'main-layout',
   async mounted() {
     if(!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
+    }
+  },
+  computed: {
+    error() {
+      return this.$store.getters.error
+    }
+  },
+  watch: {
+    error(fbError) {
+      this.$error(messages[fbError.code] || 'Что-то пошло не так')
     }
   },
   components: {
